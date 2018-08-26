d826f4bb3be003cd2f5106083d95dfc3983e88ca
@vbandrade <vbandrade@gmail.com>
2018-08-26 14:11:42 -0300

    aula 79 - contador num stateful widget

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  AppState createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          body: Center(child: Text("$counter")),
           floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
-            onPressed: () => print("Hi there!"),
+            onPressed: () {
+              setState(() {
+                counter++;
+              });
+            },
           ),
           appBar: AppBar(title: Text("Let's see Images!"))),
     );
