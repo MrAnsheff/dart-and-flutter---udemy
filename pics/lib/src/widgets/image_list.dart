6b8cbab36d22289f836e08c03053141d212fa35c
@vbandrade <vbandrade@gmail.com>
2018-08-26 16:01:03 -0300

    aula 101 - exibindo titulo da imagem

--- pics/lib/src/widgets/image_list.dart
+++ pics/lib/src/widgets/image_list.dart
@@ -10,10 +10,18 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (BuildContext context, int index) {
-        return Container(
-            margin: EdgeInsets.all(20.0),
-            child: Image.network(images[index].url));
+        return buildImage(images[index]);
       },
     );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+        decoration: BoxDecoration(border: Border.all(color: Colors.orange)),
+        padding: EdgeInsets.all(20.0),
+        margin: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+        child: Column(
+          children: <Widget>[Image.network(image.url), Text(image.title)],
+        ));
   }
 }
