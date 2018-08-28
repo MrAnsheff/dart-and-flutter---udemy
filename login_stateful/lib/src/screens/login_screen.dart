098997c29ad944a0a3a8734390a2cfbcb1889bac
@vbandrade <vbandrade@gmail.com>
2018-08-27 22:57:02 -0300

    criada tela de login

---
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
