c4f8fbca8fcd8130b40267eafef5e8409e901052
@vbandrade <vbandrade@gmail.com>
2018-09-01 21:43:38 -0300

    aula 108: exibindo texfield

--- login_stateful/lib/src/screens/login_screen.dart
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -8,6 +8,32 @@ class LoginScreen extends StatefulWidget {
 class LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+          margin: EdgeInsets.all(20.0),
+          child: Form(
+              child: Column(
+            children: [
+              _emailField(),
+              _passwordField(),
+              _submitButton(),
+            ],
+          ))),
+    );
+  }
+
+  Widget _emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: "Email address", hintText: "you@example.com"),
+    );
+  }
+
+  _passwordField() {
+    return Container();
+  }
+
+  _submitButton() {
     return Container();
   }
 }
