3da5bc91cc2f255a6ae762687d29fad467afa074
@vbandrade <vbandrade@gmail.com>
2018-09-01 21:54:19 -0300

    aula 111: exibindo botao de enviar

--- login_stateful/lib/src/screens/login_screen.dart
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -41,6 +41,9 @@ class LoginScreenState extends State<LoginScreen> {
   }
 
   _submitButton() {
-    return Container();
+    return RaisedButton(
+      child: Text("Submit!"),
+      onPressed: () {},
+    );
   }
 }
