a1741c51adaad3feb9814f9b4f76b2cb8687348f
@vbandrade <vbandrade@gmail.com>
2018-09-11 20:44:54 -0300

    aula 150 - criacao de Streams no bloc

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,3 +1,12 @@
 import "dart:async";
 
-class Bloc {}
+class Bloc {
+  final _emailController = StreamController<String>();
+  final _passwordController = StreamController<String>();
+
+  Function(String) get changeEmail => _emailController.sink.add;
+  Stream<String> get email => _emailController.stream;
+
+  Function(String) get changePassword => _passwordController.sink.add;
+  Stream<String> get password => _passwordController.stream;
+}
