3ca58dcba5f92812f5226a4027f82ff8c9612a64
@vbandrade <vbandrade@gmail.com>
2018-09-11 20:14:06 -0300

    aula 145 - exibindo componentes na tela

--- login_bloc/lib/src/screens/login_screen.dart
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+  emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: "you@example.com",
+        labelText: "Email address",
+      ),
+    );
+  }
+
+  passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Password",
+        labelText: "Password",
+      ),
+    );
+  }
+
+  submitButton() {
+    return RaisedButton(
+      child: Text("Login"),
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(margin: EdgeInsets.all(20.0), child: Column(children: []));
+    return Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(children: [
+          emailField(),
+          passwordField(),
+          Container(
+            margin: EdgeInsets.only(top: 25.0),
+          ),
+          submitButton(),
+        ]));
   }
 }
