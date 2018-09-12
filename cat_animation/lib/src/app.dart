9d632dd4c6f9ae4a863c5ecbd09be9a2c13b954d
@vbandrade <vbandrade@gmail.com>
2018-09-12 18:12:11 -0300

    aula 180 - criacao do projeto de animacao

---
+++ cat_animation/lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import "./screens/home.dart";
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Cat Animation',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
