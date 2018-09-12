64c15f14dd7c94925618c933d5b0c0f374e5674c
@vbandrade <vbandrade@gmail.com>
2018-09-12 18:54:09 -0300

    animando imagem do gato

--- cat_animation/lib/src/screens/home.dart
+++ cat_animation/lib/src/screens/home.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => new _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  @override
+  initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn,
+      ),
+    );
+
+    catController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,5 +39,14 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  buildAnimation() {}
+  buildAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      child: Cat(),
+      builder: (BuildContext context, Widget child) {
+        return Container(
+            child: child, margin: EdgeInsets.only(top: catAnimation.value));
+      },
+    );
+  }
 }
