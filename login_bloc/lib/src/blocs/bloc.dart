0260ce49bbc10c68e582a2908a374b46be97d3f2
@vbandrade <vbandrade@gmail.com>
2018-09-11 21:03:10 -0300

    aula 154 - aplicando mixin validator ao bloc

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,6 +1,7 @@
 import "dart:async";
+import "validators.dart";
 
-class Bloc {
+class Bloc extends Object with Validators {
   final _emailController = StreamController<String>();
   final _passwordController = StreamController<String>();
 
