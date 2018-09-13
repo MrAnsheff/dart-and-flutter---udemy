d0e3dea6b87d637e00d56e2ed6b71e2e87913a8d
@vbandrade <vbandrade@gmail.com>
2018-09-13 17:40:34 -0300

    aula 200 - posicionando gato no topo da caixa

--- cat_animation/lib/src/screens/home.dart
+++ cat_animation/lib/src/screens/home.dart
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     );
     print(catController.status);
 
-    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+    catAnimation = Tween(begin: -50.0, end: 100.0).animate(
       CurvedAnimation(
         parent: catController,
         curve: Curves.easeIn,
@@ -70,7 +70,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
       builder: (BuildContext context, Widget child) {
         return Positioned(
           child: child,
-          bottom: catAnimation.value,
+          top: catAnimation.value,
           right: 0.0,
           left: 0.0,
         );
