0c9272c2d3f550d4ed0aff1075947036c65a0876
@vbandrade <vbandrade@gmail.com>
2018-09-15 17:16:31 -0300

    aula 246 - implementação de classe abstrata

--- 01_deck_of_cards/lib/energy_grid.dart
+++ 01_deck_of_cards/lib/energy_grid.dart
@@ -1,21 +1,33 @@
-class PowerGrid {
-  List<NuclearPlant> connectedPlants = [];
+abstract class PowerPlant {
+  turnOn();
+}
 
-  addPlant(NuclearPlant plant) {
-    connectedPlants.add(plant);
-    plant.turnOn();
+class NuclearPlant implements PowerPlant {
+  turnOn() {
+    print("Im a nuclear plant turning on");
   }
 }
 
-class NuclearPlant {
+class SolarPlant implements PowerPlant {
   turnOn() {
-    print("Im a nuclear plant turning on");
+    print("Im a solar plant turning on");
+  }
+}
+
+class PowerGrid {
+  List<PowerPlant> connectedPlants = [];
+
+  addPlant(PowerPlant plant) {
+    connectedPlants.add(plant);
+    plant.turnOn();
   }
 }
 
 void main() {
   PowerGrid grid = PowerGrid();
   NuclearPlant plantA = NuclearPlant();
+  SolarPlant plantB = SolarPlant();
 
   grid.addPlant(plantA);
+  grid.addPlant(plantB);
 }
