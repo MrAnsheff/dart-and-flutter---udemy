4be4148a007fd3b1a066015cb4de269dbbc8f166
@vbandrade <vbandrade@gmail.com>
2018-09-15 00:09:50 -0300

    aula 221 - criando modelo

---
+++ 06_news/lib/src/models/item_model.dart
@@ -0,0 +1,55 @@
+class ItemModel {
+  //The item's unique id.
+  final int id;
+
+  //	true if the item is deleted.
+  final bool deleted;
+
+  //The type of item. One of "job", "story", "comment", "poll", or "pollopt".
+  final String type;
+
+  //The username of the item's author.
+  final String by;
+
+  //Creation date of the item, in Unix Time.
+  final int time;
+
+  //The comment, story or poll text. HTML.
+  final String text;
+
+  //true if the item is dead.
+  final bool dead;
+
+  //The comment's parent: either another comment or the relevant story.
+  final int parent;
+
+  //The ids of the item's comments, in ranked display order.
+  final List<dynamic> kids;
+
+  //The URL of the story.
+  final String url;
+
+  //The story's score, or the votes for a pollopt.
+  final int score;
+
+  //The title of the story, poll or job.
+  final String title;
+
+  //In the case of stories or polls, the total comment count.
+  final int descendants;
+
+  ItemModel.fromJson(Map<String, dynamic> parsedJson)
+      : id = parsedJson["id"],
+        deleted = parsedJson["deleted"],
+        type = parsedJson["type"],
+        by = parsedJson["by"],
+        time = parsedJson["time"],
+        text = parsedJson["text"],
+        dead = parsedJson["dead"],
+        parent = parsedJson["parent"],
+        kids = parsedJson["kids"],
+        url = parsedJson["url"],
+        score = parsedJson["score"],
+        title = parsedJson["title"],
+        descendants = parsedJson["descendants"];
+}
