e8d84555db66fe986e8fc9a06d56ca844e25826c
@vbandrade <vbandrade@gmail.com>
2018-09-15 22:55:15 -0300

    aula 271 - removendo imports desnecessários

--- 06_news/test/repository_test.dart
+++ 06_news/test/repository_test.dart
@@ -1,11 +1,8 @@
 import 'package:news/src/models/item_model.dart';
 import 'package:news/src/resources/news_api_provider.dart';
-import 'package:news/src/resources/news_db_provider.dart';
+
 import 'package:news/src/resources/repository.dart';
-import "dart:convert";
 import "package:test/test.dart";
-import "package:http/http.dart";
-import "package:http/testing.dart";
 
 void main() {
   test('create repository', () async {
