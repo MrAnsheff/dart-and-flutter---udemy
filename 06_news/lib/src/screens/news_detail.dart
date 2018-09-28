588d607fac282cf2893f11ee08c7b5d29381c365
@vbandrade <vbandrade@gmail.com>
2018-09-28 00:04:33 -0300

    aula 322: exibindo comentários

--- 06_news/lib/src/screens/news_detail.dart
+++ 06_news/lib/src/screens/news_detail.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import '../widgets/news_list_tile_shimmer.dart';
 import '../blocs/comments_provider.dart';
 import '../models/item_model.dart';
 
@@ -56,12 +57,77 @@ class NewsDetail extends StatelessWidget {
               return buildLoading();
             else {
               if (itemSnapshot.data != null)
-                return buildTitle(itemSnapshot.data.title);
+                return ListView(
+                  children: [
+                    buildTitle(itemSnapshot.data.title),
+                    buildAuthor(itemSnapshot.data.by),
+                    buildComments(bloc, itemSnapshot.data.kids, snapshot.data),
+                  ],
+                );
             }
             return buildLoading();
           },
         );
       },
     );
+  }
+
+  buildComments(bloc, List kids, Map<int, Future<ItemModel>> cache) {
+    final result = kids.map<Widget>((kid) {
+      return FutureBuilder(
+        future: cache[kid],
+        builder: (context, AsyncSnapshot<ItemModel> snapshot) {
+          if (!snapshot.hasData) {
+            return NewsListTileShimmer();
+          }
+
+          final item = snapshot.data;
+
+          final children = <Widget>[
+            ListTile(
+              title: buildText(item.text),
+              subtitle: item.by == "" ? Text("Deleted") : Text(item.by),
+              contentPadding: EdgeInsets.only(
+                right: 16.0,
+                left: 16.0,
+              ),
+            ),
+            Divider(),
+          ];
+
+          return Column(
+            children: children,
+          );
+        },
+      );
+    }).toList();
+
+    return Column(
+      children: result,
+    );
+  }
+
+  buildText(String text) {
+    return Container(
+        alignment: Alignment.topCenter,
+        margin: EdgeInsets.all(5.0),
+        child: Text(
+          text,
+          textAlign: TextAlign.justify,
+        ));
+  }
+
+  buildAuthor(String author) {
+    return Container(
+        alignment: Alignment.topLeft,
+        margin: EdgeInsets.only(left: 15.0),
+        child: Text(
+          "by: $author",
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ));
   }
 }
