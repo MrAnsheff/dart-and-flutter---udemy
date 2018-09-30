411b6441f59db32f4ee6986e5611bfe53fda07fc
@vbandrade <vbandrade@gmail.com>
2018-09-30 18:13:10 -0300

    centralizacao de setup de rotas

--- 06_news/lib/src/app.dart
+++ 06_news/lib/src/app.dart
@@ -1,32 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
 
-import 'screens/news_list.dart';
-import 'blocs/top_stories_provider.dart';
-
-import 'screens/news_detail.dart';
-import 'blocs/comments_provider.dart';
-
 import 'resources/injector.dart' as injector;
-
 import 'screens/routes.dart' as routes;
 
 class App extends StatelessWidget {
   final router = Router();
   App() {
-    // Define home page.
-    router.define(
-      '/',
-      handler: routes.homeHandler,
-    );
-
-    router.define('/news/:itemId', handler: Handler(
-        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      final itemId = int.tryParse(params["itemId"][0]);
-      if (itemId == null) return null;
-      return CommentsProvider(itemId, child: NewsDetail(itemId));
-    }));
-
+    routes.setup(router);
     injector.setup();
   }
 
@@ -34,23 +15,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "News!",
-      // onGenerateRoute: onGenerateRoute,
       onGenerateRoute: router.generator,
     );
-  }
-
-  Route onGenerateRoute(RouteSettings settings) {
-    if (settings.name.startsWith("/news")) {
-      return MaterialPageRoute(builder: (context) {
-        var itemId = int.tryParse(
-            settings.name.substring(settings.name.lastIndexOf("/") + 1));
-
-        return CommentsProvider(itemId, child: NewsDetail(itemId));
-      });
-    } else {
-      return MaterialPageRoute(builder: (context) {
-        return TopStoriesProvider(child: NewsList());
-      });
-    }
   }
 }
