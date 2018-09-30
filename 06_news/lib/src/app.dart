50a57190afd05e6a4a75be0c0e00fa2b2a9e7d5f
@vbandrade <vbandrade@gmail.com>
2018-09-30 17:57:38 -0300

    utilizando fluro como roteador do app

--- 06_news/lib/src/app.dart
+++ 06_news/lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluro/fluro.dart';
 
 import 'screens/news_list.dart';
 import 'blocs/top_stories_provider.dart';
@@ -8,8 +9,27 @@ import 'blocs/comments_provider.dart';
 
 import 'resources/injector.dart' as injector;
 
+import 'screens/routes.dart' as routes;
+
 class App extends StatelessWidget {
+  final router = Router();
   App() {
+    // Define home page.
+    router.define(
+      '/',
+      handler: Handler(
+          handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+        return TopStoriesProvider(child: NewsList());
+      }),
+    );
+
+    router.define('/news/:itemId', handler: Handler(
+        handlerFunc: (BuildContext context, Map<String, dynamic> params) {
+      final itemId = int.tryParse(params["itemId"][0]);
+      if (itemId == null) return null;
+      return CommentsProvider(itemId, child: NewsDetail(itemId));
+    }));
+
     injector.setup();
   }
 
@@ -17,7 +37,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "News!",
-      onGenerateRoute: onGenerateRoute,
+      // onGenerateRoute: onGenerateRoute,
+      onGenerateRoute: router.generator,
     );
   }
 
